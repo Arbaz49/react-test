4d0ae217691938f980f58addee582c5670395431
Arbaz Solkar <102220613+Arbaz49@users.noreply.github.com>
2025-07-14 15:21:24 +0530

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,2 +1,13 @@
+// App.test.js
+import React from 'react';
 import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+import App from './App';
 
+test('renders Home component when route is /', () => {
+  render(
+    <MemoryRouter initialEntries={['/']}>
+      <App />
+    </MemoryRouter>
+  );
+});
